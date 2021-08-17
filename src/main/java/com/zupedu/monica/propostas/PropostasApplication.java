080bc4f59b10e4bc67cf48c34aa66278f4cd9577
monicazup <monica.gomides@zup.com.br>
2021-08-17 16:19:35 -0300

    1.0 Setup do projeto

---
+++ src/main/java/com/zupedu/monica/propostas/PropostasApplication.java
@@ -0,0 +1,13 @@
+package com.zupedu.monica.propostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zupedu/monica/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupedu.monica.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
