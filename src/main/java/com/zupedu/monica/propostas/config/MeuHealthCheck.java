7513e163a825f6c9fb3a2f21cfedd2ae49ec8ae2
monicazup <monica.gomides@zup.com.br>
2021-08-18 23:32:15 -0300

    4.11.7 melhorando a visibilidade da nossa aplicação

---
+++ src/main/java/com/zupedu/monica/propostas/config/MeuHealthCheck.java
@@ -0,0 +1,30 @@
+package com.zupedu.monica.propostas.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MeuHealthCheck implements HealthIndicator {
+
+    @Override
+    public Health getHealth(boolean includeDetails) {
+        return HealthIndicator.super.getHealth(includeDetails);
+    }
+
+    @Override
+    public Health health() {
+        Map<String, Object> details = new HashMap<>();
+        details.put("descrição", "Meu primeiro Health Check customizado!");
+        details.put("versão", "1.2.3");
+        details.put("endereço", "127.0.0.1");
+
+        return Health.status(Status.UP).withDetails(details).build();
+
+    }
+}
